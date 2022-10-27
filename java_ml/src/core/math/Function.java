27f82f28dcdaf374a8c1ab579a547681b9c37fb7
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-10-27 15:12:22 +0200

    :sparkles: functions and activations

--- java_ml/src/core/math/Function.java
+++ java_ml/src/core/math/Function.java
@@ -1,5 +1,30 @@
 package core.math;
 
-public class Function {
+import core.math.linalg.Matrix;
+
+public abstract class Function {
+    public abstract double f(double x);
+
+    public abstract double df(double x);
+
+    public Matrix f(Matrix x) {
+        Matrix y = new Matrix(x.getRows(), x.getCols());
+        for (int i = 0; i < x.getRows(); i++) {
+            for (int j = 0; j < x.getCols(); j++) {
+                y.setValue(i, j, f(x.getValue(i, j)));
+            }
+        }
+        return y;
+    }
+
+    public Matrix df(Matrix x) {
+        Matrix y = new Matrix(x.getRows(), x.getCols());
+        for (int i = 0; i < x.getRows(); i++) {
+            for (int j = 0; j < x.getCols(); j++) {
+                y.setValue(i, j, df(x.getValue(i, j)));
+            }
+        }
+        return y;
+    }
 
 }
---
+++ java_ml/src/core/nn/Activation.java
@@ -0,0 +1,71 @@
+package core.nn;
+
+import core.math.Function;
+
+public abstract class Activation extends Function {
+
+    public static final Activation Sigmoid = new Activation() {
+        @Override
+        public double f(double x) {
+            return 1 / (1 + Math.exp(-x));
+        }
+
+        @Override
+        public double df(double x) {
+            return f(x) * (1 - f(x));
+        }
+    };
+
+    public static final Activation TanH = new Activation() {
+        @Override
+        public double f(double x) {
+            return Math.tanh(x);
+        }
+
+        @Override
+        public double df(double x) {
+            return 1 - Math.pow(f(x), 2);
+        }
+    };
+
+    public static final Activation ReLU = new Activation() {
+        @Override
+        public double f(double x) {
+            return Math.max(0, x);
+        }
+
+        @Override
+        public double df(double x) {
+            return x > 0 ? 1 : 0;
+        }
+    };
+
+    public static final Activation LeakyReLU = new Activation() {
+        @Override
+        public double f(double x) {
+            return x > 0 ? x : 0.01 * x;
+        }
+
+        @Override
+        public double df(double x) {
+            return x > 0 ? 1 : 0.01;
+        }
+    };
+
+    public static final Activation LeakyReLU(double alpha) {
+
+        Activation LeakyReLU = new Activation() {
+            @Override
+            public double f(double x) {
+                return x > 0 ? x : alpha * x;
+            }
+
+            @Override
+            public double df(double x) {
+                return x > 0 ? 1 : alpha;
+            }
+        };
+
+        return LeakyReLU;
+    }
+}
