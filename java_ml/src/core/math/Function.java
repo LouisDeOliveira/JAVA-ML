6127023cc546d2face44c64a4d106c290493f2c8
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-10-27 15:46:01 +0200

    :truck: moved matrix operations to activations

--- java_ml/src/core/math/Function.java
+++ java_ml/src/core/math/Function.java
@@ -1,42 +1,8 @@
 package core.math;
 
-import core.math.linalg.Matrix;
-
 public abstract class Function {
     public abstract double f(double x);
 
     public abstract double df(double x);
-
-    /**
-     * Applies the function to each element of the matrix.
-     * 
-     * @param x the matrix to apply the function to
-     * @return the resulting matrix
-     */
-    public Matrix f(Matrix x) {
-        Matrix y = new Matrix(x.getRows(), x.getCols());
-        for (int i = 0; i < x.getRows(); i++) {
-            for (int j = 0; j < x.getCols(); j++) {
-                y.setValue(i, j, f(x.getValue(i, j)));
-            }
-        }
-        return y;
-    }
-
-    /**
-     * Applies the derivative of the function to each element of the matrix.
-     * 
-     * @param x the matrix to apply the derivative of the function to
-     * @return the resulting matrix
-     */
-    public Matrix df(Matrix x) {
-        Matrix y = new Matrix(x.getRows(), x.getCols());
-        for (int i = 0; i < x.getRows(); i++) {
-            for (int j = 0; j < x.getCols(); j++) {
-                y.setValue(i, j, df(x.getValue(i, j)));
-            }
-        }
-        return y;
-    }
 
 }
--- java_ml/src/core/nn/Activation.java
+++ java_ml/src/core/nn/Activation.java
@@ -1,6 +1,7 @@
 package core.nn;
 
 import core.math.Function;
+import core.math.linalg.Matrix;
 
 public abstract class Activation extends Function {
 
@@ -80,5 +81,37 @@ public abstract class Activation extends Function {
             return x > 0 ? 1.0507 : 1.0507 * 1.67326 * Math.exp(x);
         }
     };
+
+    /**
+     * Applies the function to each element of the matrix.
+     * 
+     * @param x the matrix to apply the function to
+     * @return the resulting matrix
+     */
+    public Matrix f(Matrix x) {
+        Matrix y = new Matrix(x.getRows(), x.getCols());
+        for (int i = 0; i < x.getRows(); i++) {
+            for (int j = 0; j < x.getCols(); j++) {
+                y.setValue(i, j, f(x.getValue(i, j)));
+            }
+        }
+        return y;
+    }
+
+    /**
+     * Applies the derivative of the function to each element of the matrix.
+     * 
+     * @param x the matrix to apply the derivative of the function to
+     * @return the resulting matrix
+     */
+    public Matrix df(Matrix x) {
+        Matrix y = new Matrix(x.getRows(), x.getCols());
+        for (int i = 0; i < x.getRows(); i++) {
+            for (int j = 0; j < x.getCols(); j++) {
+                y.setValue(i, j, df(x.getValue(i, j)));
+            }
+        }
+        return y;
+    }
 
 }
