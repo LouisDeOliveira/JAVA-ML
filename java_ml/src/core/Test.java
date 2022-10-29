3fc7c5629f8510f643bdf4fa7899c84278922630
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-10-29 21:17:29 +0200

    :truck: renamed model

--- java_ml/src/core/Test.java
+++ java_ml/src/core/Test.java
@@ -16,7 +16,7 @@ public class Test {
         labelLoader.readData("./java_ml/data/t10k-labels.idx1-ubyte");
 
         OneHotEncoder encoder = new OneHotEncoder(10);
-        Sequential model = Sequential.loadModel("MNIST.model");
+        Sequential model = Sequential.loadModel("MNIST_128.model");
 
         int correct = 0;
 
